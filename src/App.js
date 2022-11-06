e6e60dbb07a36f5e84646fd96193f3df76d12c63
bfarabi <bfarab108@gmail.com>
2022-11-06 12:26:21 +0600

    react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Home from './pages/Home/Home';
 
 
 function App() {
   return (
     <div>
+      <Home></Home>
       <h1 className='bg-info'>i am ok</h1>
     </div>
   );
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -3,6 +3,7 @@ import React from 'react';
 const Navbar = () => {
   return (
     <div>
+
       <h1>i am nav</h1>
     </div>
   );
