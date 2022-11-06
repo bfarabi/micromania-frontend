9fc74b4e11d8c98be4f2d08f08cc5f8e0d7fd863
bfarabi <bfarab108@gmail.com>
2022-11-06 15:10:08 +0600

    react

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div>
       <Home></Home>
-      <h1 className='bg-info'>i am ok</h1>
+
     </div>
   );
 }
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,11 +1,45 @@
 import React from 'react';
+import './Navbar.css';
+import microLogo from '../../assets/images/micromania.png';
+import { Icon } from '@iconify/react';
 
 const Navbar = () => {
-  return (
-    <div>
+  // let menu = document.getElementById('#menu-icon');
+  // let navbar = document.querySelector('.navbar');
 
-      <h1>i am nav</h1>
-    </div>
+  // menu.onclick = () => {
+  //   menu.classList.toggle('bx-x');
+  //   navbar.classList.toggle('open');
+  // }
+  return (
+    <>
+      <header>
+        <div className='d-flex align-items-center'>
+          <div className="bx bx-menu mx-2 " id="menu-icon" />
+          <a href="#" > <img className="logo" src={microLogo} alt="" />  </a>
+
+        </div>
+
+        <ul className="navbar navbar1 ps-0">
+          <li className='live-shop'><a href="#" >Live shopping</a></li>
+          <li className='p-3'><a href="#" >Jeux Vidéo</a></li>
+          <li><a href="#">Digital</a></li>
+          <li><a href="#">occasion</a></li>
+          <li><a href="#">fanzone</a></li>
+          <li><a href="#">Magasins</a></li>
+        </ul>
+
+        <div className="main">
+          <a href="#" className="user"><i className="ri-user-fill" /></a>
+          <Icon className='mx-1' icon="bxs:shopping-bag" color="white" width="28" />
+          <Icon icon="akar-icons:search" color="white" width="25" />
+
+
+        </div>
+
+      </header>
+
+    </>
   );
 };
 
